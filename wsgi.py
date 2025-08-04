6e00de8652a67f7a9f5db3737ab563eb96b1e2da
rendianferta <rendianferta26@gmail.com>
2025-08-04 09:03:56 +0800

    Perbaikan struktur dan konfigurasi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Tambahkan ROOT folder ke sys.path agar bisa import backend
+sys.path.insert(0, os.path.dirname(__file__))
+
 from backend import create_app
 
 app = create_app()
